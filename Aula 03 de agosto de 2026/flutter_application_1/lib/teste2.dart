299a76fe6ee93c0ec18ca60872aada7c403811da
THIAGO BORSATTO DUTRA <thiagoborsattodutra14@gmail.com>
2026-08-03 21:37:16 -0300

    feat: novo projeto de teste

---
+++ Aula 03 de agosto de 2026/flutter_application_1/lib/teste2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MeuWidgetStf extends StatefulWidget {
+  const MeuWidgetStf({super.key});
+
+  @override
+  State<MeuWidgetStf> createState() => _MeuWidgetStfState();
+}
+
+class _MeuWidgetStfState extends State<MeuWidgetStf> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
